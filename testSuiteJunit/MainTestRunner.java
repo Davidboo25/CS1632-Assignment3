15bac18a46e5257b86c855e6da53a6f5d80d4723
David Anderson <David Anderson>
2017-03-15 14:20:51 -0400

    update test runner name

--- testSuiteJunit/MainTestRunner.java
+++ testSuiteJunit/MainTestRunner.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import org.junit.runner.*;
 import org.junit.runner.notification.*;
 
-public class RedditTestRunner {
+public class MainTestRunner {
     public static void main(String[] args) {
 
 	ArrayList<Class> classesToTest = new ArrayList<Class>();
